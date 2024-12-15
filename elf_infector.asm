94d4067fe2b138b149d02ceb856c7c8146309db9
Drien95 <machnik.adrien@gmail.com>
2024-12-15 09:02:59 -0500

    sys_read ok

--- elf_infector.asm
+++ elf_infector.asm
@@ -7,6 +7,13 @@ section .data
 	error_open_msg_len equ $ - error_open_msg
 	ok_open_msg db "Ouverture correcte du fichier", 0xA
 	ok_open_msg_len equ $ - ok_open_msg
+
+	; read msg syscall
+	error_read_msg db "Erreur lors de la lecture du fichier", 0xA
+	error_read_msg_len equ $ - error_read_msg
+	ok_read_msg db "Lecture correcte du fichier", 0xA
+	ok_read_msg_len equ $ - ok_read_msg
+
 section .bss
 	buffer resb buffer_size		; Je reserve un espace memoire pour mon elf
 
@@ -22,9 +29,36 @@ _start:
 	syscall
 	test rax, rax			; Check si le file est OK (rax==0)
 	js _error_open			; Gestion d'erreur pour open
-	mov r13, rax			; Si OK alors on met le fd dans r13
-	call _ok_open
+	mov r13, rax			; Si OK alors on met le fd dans r13 en attendant le syscall print
+	call _ok_open	
+	
+	; Lecture du fichier
+	; syscall read				
+	mov rax, 0			; read(
+	mov rdi, r13			; int fd,
+	mov rsi, buffer			; void buf*, 	
+	mov rdx, buffer_size		; size_t count
+	syscall				;)
+	;	gestion erreur sys_read
+	test rax, rax
+	js _error_read
+	call _ok_read
+
+
 	call _exit
+
+
+_ok_read:
+	lea rsi,  [rel ok_read_msg]
+	mov rdx, ok_read_msg_len
+	call _print_msg
+	ret
+
+_error_read:
+	lea rsi, [rel error_read_msg]
+	mov rdx, error_read_msg_len
+	call _print_msg
+	call _exit	
 
 _ok_open:
 	lea rsi, [rel ok_open_msg]
